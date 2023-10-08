2c786ae880685278a6e6bd7d91b0ae5de3af6a70
lxstnxmad <osnovnoymoney@gmail.com>
2023-10-08 14:50:02 +0600

    updated

---
+++ solution.js
@@ -0,0 +1,152 @@
+function checkQuiz1() {
+    var radios = document.getElementsByName("quiz1");
+    var result = document.getElementById("result1");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "2") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz2() {
+    var radios = document.getElementsByName("quiz2");
+    var result = document.getElementById("result2");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "3") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz3() {
+    var radios = document.getElementsByName("quiz3");
+    var result = document.getElementById("result3");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "1") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz4() {
+    var radios = document.getElementsByName("quiz4");
+    var result = document.getElementById("result4");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "2") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz5() {
+    var radios = document.getElementsByName("quiz5");
+    var result = document.getElementById("result5");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "3") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz6() {
+    var radios = document.getElementsByName("quiz6");
+    var result = document.getElementById("result6");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "1") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz7() {
+    var radios = document.getElementsByName("quiz7");
+    var result = document.getElementById("result7");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "2") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz8() {
+    var radios = document.getElementsByName("quiz8");
+    var result = document.getElementById("result8");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "3") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
+
+function checkQuiz9() {
+    var radios = document.getElementsByName("quiz9");
+    var result = document.getElementById("result9");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "1") {
+                result.innerHTML = "Правильно!";
+                result.style.color = "green";
+            } else {
+                result.innerHTML = "Неправильно.";
+                result.style.color = "red";
+            }
+            break;
+        }
+    }
+}
